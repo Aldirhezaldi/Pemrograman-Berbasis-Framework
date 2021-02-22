739e8d6c2b3ef12f9ae96f915596142379e9b537
Aldirhezaldi <aldirhezaldi339@gmail.com>
2021-02-22 16:01:20 +0700

    push tugas

---
+++ week 2/template.js
@@ -0,0 +1,9 @@
+let fname = 'Cristian';
+let lname = 'Ronaldo';
+let age = prompt("Masukkan umur Cristian Ronaldo !");
+
+// Cara lama
+// let result
+
+let result = `${fname} ${lname} is ${age} years old`;
+alert(result);
