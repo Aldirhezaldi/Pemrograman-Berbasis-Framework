b6b939b6ff7a25661cd4566283a3ef81338c34b2
Aldirhezaldi <aldirhezaldi339@gmail.com>
2021-04-19 12:28:39 +0700

    push modul 10
    push modul 10

---
+++ week10/modul10/src/firebase.config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDbIVdUetsURGhhGmkUWpCqL6C7jNtX_4w",
+    authDomain: "module-10-814ef.firebaseapp.com",
+    projectId: "module-10-814ef",
+    storageBucket: "module-10-814ef.appspot.com",
+    messagingSenderId: "118260664372",
+    appId: "1:118260664372:web:236bd9c6c15da23291a261",
+    measurementId: "G-6JTPZXVG28"
+  };
+
+export default firebaseConfig;
